43e2002076633cd6e3fbf4b42199e35db0b481d4
Travis Hunsberger <tfhunsberger@gmail.com>
2023-05-17 17:48:13 -0400

    firebase config setup, initial project setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAe5yr-RBOBKVJITOxypxWaHXQgD9WSbE4",
+  authDomain: "slack-clone-tfh12c.firebaseapp.com",
+  projectId: "slack-clone-tfh12c",
+  storageBucket: "slack-clone-tfh12c.appspot.com",
+  messagingSenderId: "163141759056",
+  appId: "1:163141759056:web:414ed14f16a10a89952155"
+};
