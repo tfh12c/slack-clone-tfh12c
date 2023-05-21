b5b0995542a56be592b02153aa373aee92dceb15
Travis Hunsberger <tfhunsberger@gmail.com>
2023-05-20 22:02:57 -0400

    messages formatted and styled

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,6 +7,7 @@ import { selectRoomId } from "../features/appSlice";
 import ChatInput from "./ChatInput";
 import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
 import { db } from "../firebase";
+import Message from "./Message";
 
 function Chat() {
 	const roomId = useSelector(selectRoomId);
@@ -28,7 +29,19 @@ function Chat() {
 			</Header>
 
 			<ChatMessages>
-				{/* list out chat messages here */}
+				{roomMessage?.docs.map((doc) => {
+					const { message, timestamp, user, userImage } = doc.data();
+
+					return (
+						<Message 
+							key={doc.id}
+							message={message}
+							timestamp={timestamp}
+							user={user}
+							userImage={userImage}
+						/>
+					)
+				})}
 			</ChatMessages>
 
 			<ChatInput
